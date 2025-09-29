d195fbd93670a58e866436b4b490318945ab09fe
TNTStudios <tntstudiosn@gmail.com>
2025-09-28 18:45:53 -0600

    4

---
+++ src/client/java/com/TNTStudios/ev/client/EvClient.java
@@ -0,0 +1,10 @@
+package com.TNTStudios.ev.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EvClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/TNTStudios/ev/Ev.java
@@ -0,0 +1,10 @@
+package com.TNTStudios.ev;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Ev implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
